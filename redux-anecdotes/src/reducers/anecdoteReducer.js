0dc323b22d810639e3115da9279bad7e31293397
hasferrr <79697518+hasferrr@users.noreply.github.com>
2024-01-09 06:13:52 +0700

    finish 6.16 - async actions redux thunk

--- redux-anecdotes/src/reducers/anecdoteReducer.js
+++ redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -1,4 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit'
+import anecdoteService from '../services/anecdotes'
 
 const anecdoteSlice = createSlice({
   name: 'anecdote',
@@ -20,4 +21,10 @@ const anecdoteSlice = createSlice({
 
 export const { incrementVote, createAnecdote, setAnecdotes } =
   anecdoteSlice.actions
+
+export const initializeAnecdotes = () => async (dispatch) => {
+  const anecdotes = await anecdoteService.getAll()
+  dispatch(setAnecdotes(anecdotes))
+}
+
 export default anecdoteSlice.reducer
